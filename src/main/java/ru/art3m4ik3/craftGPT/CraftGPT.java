2f22cf65c5ba779f1b636c320a34a219a8b7a4ae
art3m4ik3 <art3m4ik3@gmail.com>
2025-03-04 14:09:32 +0300

    first commit

---
+++ src/main/java/ru/art3m4ik3/craftGPT/CraftGPT.java
@@ -0,0 +1,17 @@
+package ru.art3m4ik3.craftGPT;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftGPT extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
